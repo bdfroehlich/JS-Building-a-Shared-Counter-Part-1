d7e91e9a5bfba7673f390cb7667100f9c514e873
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-01-22 11:25:49 -0600

    adding steps up to bonus - counter not working on secondary device

--- public/index.js
+++ public/index.js
@@ -1,9 +1,16 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('refresh-button');
 
-    let countValue = 0;
+
+    let localFetch = await fetch('http://localhost:9001/counter')
+    let fetchResponse = await localFetch.json();
+    console.log(fetchResponse);
+
+
+    let countValue = fetchResponse.value;
 
     function increment(){
         countValue++;
@@ -15,8 +22,13 @@ function main(){
         countContainer.textContent = countValue;
     }
 
+    // function refresh(){
+    //     countContainer.textContent = countValue;
+    // }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    // refreshButton.addEventListener('click',refresh);
     countContainer.textContent = countValue;
 }
 main()
